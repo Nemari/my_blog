dec21b5e7af41ec9140348eb6fc34cf3d6bf2c28
Nemari <baksfyl@gmail.com>
2019-11-28 21:51:36 +0000

    Changing settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+9%3#x7ol^t3@cp5n^rlzg=2hbw)hgqyh&dkp7kj7z21qm80hb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<Nemari>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'nemari.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
